5ebc2015bd6a7333f3c10402c544a48c711c11ba
Jono Spiro <github@lostinrecursion.net>
2016-11-01 14:56:20 -0700

    stop generating eudev man docs

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,8 @@
 
 autoreconf -f -i -s
 
-cd man
-./make.sh
+# @sarietta: there are some dependencies to build the man pages for this library
+# eudev that are not part of any other library's dependencies.
+# We don't need the man pages and these dependencies are so specific.
+# cd man
+# ./make.sh
